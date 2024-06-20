c23d0facba9b129a4a66996310a8f6e04b7d05e8
shin-na-ra <nas221@naver.com>
2024-06-20 11:40:55 +0900

    2024.06.20 snr : 프로젝트 생성 및 색 설정

---
+++ LetMeKnow/LetMeKnow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LetMeKnow
+//
+//  Created by 신나라 on 6/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LetMeKnow/LetMeKnow/LetMeKnowApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetMeKnowApp.swift
+//  LetMeKnow
+//
+//  Created by 신나라 on 6/20/24.
+//
+
+import SwiftUI
+
+@main
+struct LetMeKnowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
