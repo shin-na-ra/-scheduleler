6bb87ef1f7da2adb7b31cfcc3fbd2317318b75df
shin-na-ra <nas221@naver.com>
2024-06-20 12:23:32 +0900

    2024.06.20 snr : ContentView 생성

--- LetMeKnow/LetMeKnow/ContentView.swift
+++ LetMeKnow/LetMeKnow/ContentView.swift
@@ -5,19 +5,51 @@
 //  Created by 신나라 on 6/20/24.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var currentDate: Date = Date()
+    @State var isAlert = false            // actionSheet 실행
+    @State var isSubAlert = false            // subAlert 실행
+    @State var isResultTrue = false
+    @State var isResultFalse = false
+    @State var task: String = ""                // 입력받을 일정 변수
+    @FocusState var isTextFieldFocused: Bool    // 키보드 focus
+    @State var date: Date = Date()              // 선택된 날짜 변수
+    @State var time: Date = Date()              // 선택된 시간 변수
+    let dbModel = CalendarDB()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        ScrollView(.vertical, showsIndicators: false) { // showsIndicators : false => 스크롤바 안보이게
+            
+            VStack(spacing: 20, content: {
+                
+                //Custom Picker View
+                //CustomDatePicker(currentDate: $currentDate, dateValue: $currentDate)
+                
+            })//VStack
+            .padding(.vertical)
+        }//ScrollView
+    }//body
+    
+    
+    /* 날짜 체크 */
+    func isSameDay(date1: Date, date2: Date) -> Bool {
+        let calendar = Calendar.current
+        return calendar.isDate(date1, inSameDayAs: date2)
     }
 }
+
+    private var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter
+    }
+
 
 #Preview {
     ContentView()
